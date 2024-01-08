83d72d215accbdcdc7e2de5b1325caed66ae9459
Filip Gaj <f.gaj16@gmail.com>
2024-01-08 22:31:31 +0100

    add functions and tests

---
+++ R/basic_functions.R
@@ -0,0 +1,50 @@
+library(data.table)
+
+#' Load Data from Different Formats
+#'
+#' The \code{load_data} function reads data from files in different formats using the \code{data.table} package in R.
+#'
+#' @param file_path A character string specifying the path to the file to be loaded.
+#' @param file_format A character string specifying the format of the file. Supported formats are 'csv' and 'excel'.
+#'
+#' @return A \code{data.table} containing the loaded data.
+#'
+#' @export
+#'
+load_data <- function(file_path, file_format) {
+  if (file_format == "csv") {
+    data <- fread(file_path)
+  } else if (file_format == "excel") {
+    data <- fread(file_path, sheet = 1)
+  } else {
+    stop("Unsupported format. Supported formats are 'csv' and 'excel'")
+  }
+
+  return(data)
+}
+
+
+#' Select Columns from a Data.Table
+#'
+#' The \code{select_columns} function is designed to extract specific columns from a data.table in R.
+#'
+#' @param data A data.table from which columns are to be selected.
+#' @param columns_to_select A character vector containing the names of the columns to be selected.
+#'
+#' @return A new data.table containing only the selected columns.
+#'
+#' @export
+#'
+select_columns <- function(data, columns_to_select) {
+  if (!is.data.table(data)) {
+    stop("Input 'data' must be a data.table.")
+  }
+
+  if (any(!columns_to_select %in% colnames(data))) {
+    stop("Not all specified columns exist in the table.")
+  }
+
+  selected_data <- data[, columns_to_select, with = FALSE]
+
+  return(selected_data)
+}
---
+++ tests/testthat/basic_functions_tests.R
@@ -0,0 +1,25 @@
+library(dt.basics)
+library(testthat)
+
+example_data <- data.table(A = c(1, 2, 3), B = c("a", "b", "c"), C = c(10, 20, 30))
+
+test_that("select_columns selects columns correctly", {
+
+  selected_data_one_col <- select_columns(example_data, "A")
+  expect_equal(selected_data_one_col, data.table(A = c(1, 2, 3)))
+
+  selected_data_multiple_cols <- select_columns(example_data, c("A", "C"))
+  expect_equal(selected_data_multiple_cols, data.table(A = c(1, 2, 3), C = c(10, 20, 30)))
+
+  selected_data_all_cols <- select_columns(example_data, c("A", "B", "C"))
+  expect_equal(selected_data_all_cols, example_data)
+})
+
+test_that("select_columns handles invalid input", {
+
+  expect_error(select_columns(data.frame(A = c(1, 2, 3)), "A"),
+               "Input 'data' must be a data.table.")
+
+  expect_error(select_columns(example_data, c("A", "Nonexistent")),
+               "Not all specified columns exist in the table.")
+})
