9b4664525e7529f279f380c31e35fa29a9ebff83
Filip Gaj <f.gaj16@gmail.com>
2024-01-10 13:36:53 +0100

    almost everything work

--- R/basic_functions.R
+++ R/basic_functions.R
@@ -45,7 +45,7 @@ select_columns <- function(data, columns_to_select) {
     stop("Not all specified columns exist in the table.")
   }
 
-  selected_data <- data[, columns_to_select]
+  selected_data <- data[, columns_to_select, with = F]
 
   return(selected_data)
 }
--- tests/testthat/basic_functions_tests.R
+++ tests/testthat/basic_functions_tests.R
@@ -7,7 +7,7 @@ example_data <- data.table(A = c(1, 2, 3), B = c("a", "b", "c"), C = c(10, 20, 3
 test_that("select_columns selects columns correctly", {
 
   selected_data_one_col <- select_columns(example_data, "A")
-  expect_equal(selected_data_one_col, c(1, 2, 3))
+  expect_equal(selected_data_one_col, data.table("A" = c(1, 2, 3)))
 
   selected_data_multiple_cols <- select_columns(example_data, c("A", "C"))
   expect_equal(selected_data_multiple_cols, data.table(A = c(1, 2, 3), C = c(10, 20, 30)))
